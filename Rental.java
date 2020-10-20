672dce073da44390ae002ad9d9140570eb76572a
Marco Túlio <marcotmotta@gmail.com>
2020-10-20 15:07:50 -0300

    Commit 6

--- Rental.java
+++ Rental.java
@@ -2,16 +2,16 @@ public class Rental {
 
     private Movie _movie;
     private int _daysRented;
- 
+
     public Rental(Movie movie, int daysRented) {
        _movie = movie;
        _daysRented = daysRented;
     }
- 
+
     public int getDaysRented() {
        return _daysRented;
     }
- 
+
     public Movie getMovie() {
        return _movie;
     }
@@ -34,5 +34,13 @@ public class Rental {
                 break;
         }
         return thisAmount;
-     }
- }
+    }
+
+    public int getFrequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+            return 2;
+
+        return 1;
+    }
+}
