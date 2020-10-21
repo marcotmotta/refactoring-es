79ed63f20529327a048b4831bd780a0a68558489
Marco Túlio <marcotmotta@gmail.com>
2020-10-20 21:24:33 -0300

    Commit 14

--- Movie.java
+++ Movie.java
@@ -7,30 +7,30 @@ public class Movie {
     private String _title;
     private Price _price;
 
-   public Movie(String name, int priceCode) {
-      _title = name;
-      setPriceCode(priceCode);
-   }
+    public Movie(String name, int priceCode) {
+        _title = name;
+        setPriceCode(priceCode);
+    }
 
-   public int getPriceCode() {
-      return _price.getPriceCode();
-   }
+    public int getPriceCode() {
+        return _price.getPriceCode();
+    }
 
-   public void setPriceCode(int arg) {
-      switch (arg) {
-         case REGULAR:
-            _price = new RegularPrice();
-            break;
-         case CHILDRENS:
-            _price = new ChildrensPrice();
-            break;
-         case NEW_RELEASE:
-            _price = new NewReleasePrice();
-            break;
-         default:
-            throw new IllegalArgumentException("Incorrect Price Code");
-      }
-   }
+    public void setPriceCode(int arg) {
+        switch (arg) {
+            case REGULAR:
+                _price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                _price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                _price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
+    }
 
     public String getTitle (){
         return _title;
@@ -41,10 +41,7 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented){
-        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1)
-            return 2;
-
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 
 }
